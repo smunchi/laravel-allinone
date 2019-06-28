cee9dbd3ec507e27f76625e2f5b9aef4e970cd94
Md. Salahuddin <salah_cse_mbstu@yahoo.com>
2019-06-28 11:44:53 +0600

    Add try catch, database transaction and log

--- app/Http/Controllers/TicketController.php
+++ app/Http/Controllers/TicketController.php
@@ -4,6 +4,8 @@ namespace App\Http\Controllers;
 
 use App\Jobs\CreateTicket;
 use App\Http\Requests\CreateTicketRequest;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Log;
 
 class TicketController extends Controller
 {
@@ -19,7 +21,17 @@ class TicketController extends Controller
             'description' => 'required'
         ]);
 
-        CreateTicket::dispatch($data, Auth()->user()->id)->onQueue('user_'.Auth()->user()->id);
+        DB::transaction();
+
+        try {
+            Log::info('Calling CreateTicket job');
+            CreateTicket::dispatch($data, Auth()->user()->id)->onQueue('user_'.Auth()->user()->id);
+            DB::commit();
+        } catch (\Exception $exception) {
+            Log::error('Line: ' . $exception->getLine() . ' File: ' . $exception->getFile() . ' Error: ' . $exception->getMessage());
+
+            DB::rollBack();
+        }
 
         return redirect('/home')->with('success', 'New support ticket has been created! Wait sometime to get resolved');
     }
